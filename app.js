5249571424c78a1eea3ea50b5e037718f60e691f
Abdulaziz20007 <lil.gangsser@gmail.com>
2025-01-17 16:20:05 +0500

    ok

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const config = require("config");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const mainRouter = require("./routes/index.routes");
+const viewRouter = require("./routes/view.routes");
+const app = express();
+
+const PORT = config.get("port") || 3300;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api", mainRouter);
+app.use("/", viewRouter);
+
+app.listen(PORT, async () => {
+  try {
+    await mongoose.connect(config.get("dbUri"));
+    console.log(`Server is running on port ${PORT}`);
+  } catch (error) {
+    console.log(error);
+  }
+});
