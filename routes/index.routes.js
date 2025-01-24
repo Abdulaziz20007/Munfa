a2d0505804ec35d019f00b2d009f4a63512c15da
Abdulaziz20007 <lil.gangsser@gmail.com>
2025-01-24 19:59:51 +0500

    user admin

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const userRoutes = require("./user.routes");
 const productRoutes = require("./product.routes");
+const adminRoutes = require("./admin.routes");
 
 router.use("/user", userRoutes);
 router.use("/product", productRoutes);
+router.use("/admin", adminRoutes);
 
 module.exports = router;
